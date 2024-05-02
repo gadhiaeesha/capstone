f13132f6d0dc160a8a7a727562ee8145edd834d6
Eesha Gadhia <gadhiaeesha@gmail.com>
2024-05-02 14:38:45 -0700

    fixed menu icon display

--- frontend/lib/Widgets/app_bar.dart
+++ frontend/lib/Widgets/app_bar.dart
@@ -3,17 +3,21 @@ import 'package:flutter/material.dart';
 
 AppBar customAppBar() {
   return AppBar(
-    centerTitle: true,
+    //centerTitle: true,
     title: const Text(
       "ReMu", 
       style: TextStyle(
           color: Colors.white,
       )
     ),
-    leading: const IconButton(
-      onPressed: null, //come back to create navigation to menupage
-      icon: Icon(Icons.menu),
-      color: Colors.white,
+    leading: IconButton(
+      onPressed: () {
+        // Add functionality for the menu button here
+      },
+      icon: const Icon(
+        Icons.menu,
+        color: Colors.white, // Set the color of the menu icon to white
+      ),
     ),
     elevation: 0.0,
     backgroundColor: const Color(0xFF001133),
